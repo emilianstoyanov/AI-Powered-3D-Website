13149d495bd2c973fd81e918b9cd22a4ea4f0fbe
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-31 17:59:07 +0300

    home page

--- threejs/client/src/pages/Home.jsx
+++ threejs/client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import { motion, AbimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
+import state from '../store';
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -9,8 +10,22 @@ import {
 } from '../config/motion';
 
 const Home = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home' {...slideAnimation('left')}>
+          <motion.header>
+            <img 
+              src='./threejs.png'
+              alt='logo'
+              className='w-8 h-8 object-contain'
+            />
+          </motion.header>
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
