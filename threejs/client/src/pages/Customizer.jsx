d54303e3bcb032da982c0ac0018e7347b1ee3f82
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-31 21:11:58 +0300

    create AIPicker and update customizer

--- threejs/client/src/pages/Customizer.jsx
+++ threejs/client/src/pages/Customizer.jsx
@@ -2,16 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
-import config from '../config/config';
+// import config from '../config/config';
 import state from '../store';
 import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
-import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 
-import config from ''
+
 
 const Customizer = () => {
   return (
