e79f6a41205c939cda169822044f364c73763e8d
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-31 18:25:36 +0300

    update home page components

--- threejs/client/src/pages/Home.jsx
+++ threejs/client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
+import { CustomButton } from '../components';
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -23,13 +24,25 @@ const Home = () => {
               className='w-8 h-8 object-contain'
             />
           </motion.header>
-
           <motion.div className='home-content' {...
             headContentAnimation}>
               <motion.div {...headTextAnimation}>
                 <h1 className='head-text'>
                   LET'S <br className='xl:block hidden' /> DO IT.
                 </h1>
+              </motion.div>
+              <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+              >
+                <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your unique and exclusive shirt with our brand-new 3D customization tool. 
+                <strong>Unleash your imagination</strong>{" "} and define your own style.
+                </p>
+
+                <CustomButton
+                
+                />
               </motion.div>
           </motion.div>
         </motion.section>
