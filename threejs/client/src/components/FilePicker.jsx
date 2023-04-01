7918d756e25b9d151f415fa700ffa896aec78728
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-01 17:32:54 +0300

    color

--- threejs/client/src/components/FilePicker.jsx
+++ threejs/client/src/components/FilePicker.jsx
@@ -11,8 +11,30 @@ const FilePicker = ({ file, setFile, readFile }) => {
         type='file'
         accept='image/*'
         onChange={(e) => setFile(e.target.files[0])}
-        >
-        </input>
+        />
+          <label htmlFor='file-upload' className='filepicker-label'>
+            Upload FileS
+          </label>
+
+          <p className='mt-2' text-gray-500 text-xs truncate>
+            {file === '' ? 'No file selected' : file.name }
+          </p>
+      </div>
+
+      <div className='mt-4 flex flex-wrap gap-36'>
+        <CustomButton 
+          type='outline'
+          title='Logo'
+          handleClick={() => readFile('logo')}
+          customStyles='text-xs'
+        />
+         <CustomButton 
+          type='filled'
+          title='Full'
+          handleClick={() => readFile('full')}
+          customStyles='text-xs'
+        />
+
       </div>
     </div>
   )
