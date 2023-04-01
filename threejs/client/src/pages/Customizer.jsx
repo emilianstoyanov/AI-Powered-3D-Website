377237db961f7f5fcff37adbe6c82d158edfb26a
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-01 08:40:13 +0300

    dynamic movement

--- threejs/client/src/pages/Customizer.jsx
+++ threejs/client/src/pages/Customizer.jsx
@@ -8,7 +8,7 @@ import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
-
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 
 
