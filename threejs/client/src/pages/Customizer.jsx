06257e4cbc857802fdec313ff103af1b314ec6b8
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-01 09:28:37 +0300

    up and down buttons

--- threejs/client/src/pages/Customizer.jsx
+++ threejs/client/src/pages/Customizer.jsx
@@ -10,10 +10,8 @@ import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
-
-
 const Customizer = () => {
-  const snap  =useSnapshot(state);
+  const snap  = useSnapshot(state);
 
   return (
     <AnimatePresence>
@@ -35,6 +33,33 @@ const Customizer = () => {
               ))}
             </div>
           </div>
+        </motion.div>
+
+        <motion.div
+          className='absolute z-10 top-5 right-5'
+          {...fadeAnimation}
+        >
+          <CustomButton 
+            type='filled'
+            title='Go Back'
+            handleClick={() => state.intro = true}
+            customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+          />
+        </motion.div>
+
+        <motion.div
+          className='filtertabs-container'
+          {...slideAnimation('up')}
+        >
+           {FilterTabs.map((tab) => (
+                <Tab 
+                  key={tab.name}
+                  tab={tab}
+                  isFilterTab
+                  isActiveTab=''
+                  handleClick={() => {}}
+                />
+              ))}
         </motion.div>
         </>
       )}
