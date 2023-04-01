9e03ba815c34fc059b0ddd6d481e6f011e8c0b56
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-01 08:57:13 +0300

    create tab left

--- threejs/client/src/pages/Customizer.jsx
+++ threejs/client/src/pages/Customizer.jsx
@@ -19,7 +19,23 @@ const Customizer = () => {
     <AnimatePresence>
       {!snap.intro && (
         <>
-        Costumizer
+        <motion.div
+        key='custom'
+        className='absolute top-0 left-0 z-10'
+        {...slideAnimation('left')}
+        >
+          <div className='flex items-center min-h-screen'>
+            <div className='editortabs-container tabs'>
+              {EditorTabs.map((tab) => (
+                <Tab 
+                  key={tab.name}
+                  tab={tab}
+                  handleClick={() => {}}
+                />
+              ))}
+            </div>
+          </div>
+        </motion.div>
         </>
       )}
     </AnimatePresence>
