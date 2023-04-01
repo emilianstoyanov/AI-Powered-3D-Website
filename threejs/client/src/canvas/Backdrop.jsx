d1025a5835b622cff04cde7145deb16d22fa0867
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-04-01 11:51:50 +0300

    update backdrop

--- threejs/client/src/canvas/Backdrop.jsx
+++ threejs/client/src/canvas/Backdrop.jsx
@@ -5,12 +5,20 @@ import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 
 const Backdrop = () => {
+  const shadows = useRef();
+
   return (
     <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scae={10}
+      rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
       <RandomizedLight 
-        amount={4}
+        // amount={4}
       />
     </AccumulativeShadows>
   )
--- threejs/client/src/canvas/index.jsx
+++ threejs/client/src/canvas/index.jsx
@@ -9,10 +9,13 @@ const CanvasModel = () => {
   return (
     <Canvas
       shadows
-      camera={{ position: [0, 0, 0], fov: 25}}
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className='w-full max-w-full h-full transition-all ease-in'
     >
       <ambientLight intensity={0.5} />
       <Environment preset='city' />
+      
       <CameraRig>
         <Backdrop />
         <Center>
